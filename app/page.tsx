66195e9b815ea81e2ab88ba552bfa4686447f423
rafrafciooo <drweedel@gmail.com>
2025-01-29 23:59:57 +0100

    start

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div className="flex-center h-screen">
+  <h1 className="text-3xl font-bold text-brand">
+    CookieCloud - Twój prywatny dysk.
+  </h1>
+</div>
+  );
+}
